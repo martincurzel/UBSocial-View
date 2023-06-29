a0bf65337d918985238a0afcf891f77ab0f06625
Nicolas Margossian <nmargo@pm.me>
2023-06-29 01:12:49 -0300

    proposal

---
+++ src/helpers/apiCallRead.js
@@ -0,0 +1,14 @@
+import axios from 'axios';
+
+const BASE_PATH = process.env.REACT_APP_CONTROLLER_API_BASE_PATH;
+
+export const callApiRead = (url) => {
+  console.log(BASE_PATH+url)
+    return axios({
+      method: "GET",
+      url: BASE_PATH + url,
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+};
--- src/pages/PropuestasPage.js
+++ src/pages/PropuestasPage.js
@@ -1,12 +1,85 @@
 import React from 'react';
+import { callApiRead } from '../helpers/apiCallRead';
+import { useState, useEffect } from 'react';
+import { useSelector } from "react-redux";
+import Button from '@mui/material/Button';
+import Modal from '@mui/material/Modal';
+
+
+
 
 const PropuestasPage = () => {
+
+  const [proposals, setProposals] = useState([]);
+  const isLoggedIn = useSelector((state) => state.auth.isLoggedIn);
+  const [isLoginModalOpen, setIsLoginModalOpen] = useState(false);
+
+
+  useEffect(() => {
+    const fetchData = async () => {
+
+      await callApiRead("Proposal/0")
+        .then(response => {
+          setProposals(response.data)
+          console.log(response)
+        })
+        .catch(error => {
+          // Handle any errors from the API
+          console.error('Error:', error);
+        });
+
+    }
+
+    fetchData();
+
+  }, []);
+
+  const handleLoginModal = (bool) => {
+    setIsLoginModalOpen(bool);
+  };
+
   return (
     <div>
-      <h1>Welcome to the PropuestasPage</h1>
-      <p>This is the content of the PropuestasPage.</p>
+      <div className='row'>
+        <div className='col-md-10'>
+          <h1 className='mb-5'>Propuestas de la Universidad de belgrano</h1>
+        </div>
+        <div className='col-md-2 mt-3'>
+          {isLoggedIn ? (
+            <>
+              <Button variant="outlined" color="secondary">Crear</Button>
+            </>
+          ) : (<>
+              <Button onClick={() => handleLoginModal(true)} variant="outlined" color="secondary">Crear</Button>
+          </>)}
+        </div>
+      </div>
+
+
+      {proposals.map((proposal, index) => (
+        <div key={index}>
+          <hr />
+
+          <h3>{proposal.title}</h3>
+          <p>{proposal.description}</p>
+        </div>
+      ))}
+
+      <Modal open={isLoginModalOpen} onClose={() => handleLoginModal(false)}>
+        <div style={{ backgroundColor: '#f0f0f0', width: 300, height: 280, margin: 'auto', marginTop: 100, padding: 20 }}>
+          <h4>Porfavor hace log in </h4>
+          <hr/>
+          <Button variant="outlined" onClick={() =>handleLoginModal(false)} color="secondary">Cerrar</Button>
+        </div>
+      </Modal>
+
+      
     </div>
+
+
+
   );
 };
+
 
 export default PropuestasPage;
