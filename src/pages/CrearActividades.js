1e57189afaaf6be396d4a3282d59e42457883984
MateoFrontera <mateo.frontera@comunidad.ub.edu.ar>
2023-07-01 04:11:47 -0300

    mejora
    vamooo

--- src/pages/CrearActividades.js
+++ src/pages/CrearActividades.js
@@ -1,24 +1,46 @@
 import React from "react";
 import { useState } from "react";
-import { callApiNoRead } from "../helpers/apiCallNoRead";
+import { callApiNoReadFormData } from "../helpers/apiCallNoRead";
+import { Button } from "@mui/material";
 
 
 
 const CrearActividades = () => {
-    const [titulo, setTitulo] = useState("");
-    const [desc, setDesc] = useState("");
+    const [Title, setTitulo] = useState("");
+    const [Description, setDesc] = useState("");
+    const [File, setFoto] = useState("");
+    const [Contact, setContancto] = useState("");
+    const [ActivityDateFinished, setFecha] = useState ("");
+    const [image, setImage] = useState(null);
+    const [preview, setPreview] = useState(null);
     
     
+    
+    function handleImageUpload(e) {
+        const file = e.target.files[0];
+        if (file && file.type.substr(0, 5) === "image") {
+          setImage(file);
+        } else {
+          setImage(null);
+        }
+      }
+      function handlesubmit(e) {
+        e.preventDefault();
+        setPreview(URL.createObjectURL(image));
+        const formData = new FormData();
+        formData.append('Title', Title);
+        formData.append('Description', Description);
+        formData.append('Contact',Contact)
+        formData.append('ActivityDateFinished', '25-09-2023')
+        formData.append('File', image)
 
-    const handlesubmit = (e) => {
-        e.preventDefault()
-        const actividad = {titulo, desc}
+        
+        for (let pair of formData.entries()) {
+            console.log(pair[0]+ ', '+ pair[1]); 
 
-        callApiNoRead("POST","Activity", actividad)
-        .then (()=>{
-            // history.push('/')
-        })
-    }
+        }
+        callApiNoReadFormData("POST","Activity", formData)
+      }
 
 
 
@@ -31,22 +53,50 @@ const CrearActividades = () => {
                 <input 
                 type="text"
                 required 
-                value={titulo}
+                value={Title}
                 onChange={(e) => setTitulo(e.target.value)}
                 />
                 <label >Descripcion:</label>
                 <textarea
                 required
-                value={desc}
+                value={Description}
                 onChange={(e) => setDesc(e.target.value)}  
                 />
-                <label >Encargado de la Actividad:</label>
+                <label>Contacto</label>
+                <input
+                 type="text"
+                 required 
+                 value={Contact}
+                 onChange={(e) => setContancto(e.target.value)}
+                /> 
+                <label>Fecha de Finalizacion</label>
+                <input
+                type="date"
+                required 
+                value={ActivityDateFinished}
+                onChange={(e) => setFecha(e.target.value)}
+                />
+                <label >Foto de la Actividad</label>
+                <Button
+                variant="contained"
+                component="label"
+                
+                >
 
+                Subir Archivo
+                <input type="file" accept="image/*" onChange={handleImageUpload} />
+
+
+                </Button>
+                
                 <button>Crear Actividad</button>
                 <h2>Preview</h2>
-                <p>{titulo}</p>
-                <p>{desc}</p>
-               
+                <p>{Title}</p>
+                <p>{Description}</p>
+                <p>{Contact}</p>
+                <p>{ActivityDateFinished}</p>
+                <p>{File}</p>
+                {preview && <img src={preview} alt="Preview" />}
             </form>
         </div>
     );
