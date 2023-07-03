148a2211758598931161684ff6f482c1d77a5066
mcurzel <martin.curzel@contractors.lesschwab.com>
2023-07-03 00:25:52 -0300

    UI enhancements in Perfil

--- src/components/MisActividades.js
+++ src/components/MisActividades.js
@@ -1,14 +1,16 @@
 import React, { useState, useEffect } from 'react';
 import { callApiNoRead } from "../helpers/apiCallNoRead";
-import { Button } from '@mui/material';
+import { Button, Box } from '@mui/material';
 import Grid from '@mui/material/Grid';
 import Typography from '@mui/material/Typography';
 import { Link } from 'react-router-dom';
 import Swal from 'sweetalert2';
 import withReactContent from "sweetalert2-react-content";
+import { format } from "date-fns";
+
 
 const MisActividades = () => {
-  const [activities, setActivities]= useState([]);
+  const [activities, setActivities]= useState(null);
   const loginToken = localStorage.getItem('jwtToken');
 
   const fetchActivities = () => {
@@ -55,24 +57,36 @@ const MisActividades = () => {
 
   return (
     <div style={{ width: 'auto', margin: 'auto' }}>
-      {activities.length > 0 ? (
+      {activities == null ? (
+      <>
+        <span>cargando...</span>
+      </>
+      ) : activities.length > 0 ? (
         <>
+        <Typography component={'span'} variant={'h2'} sx={{ color: '#372249' }}>Mis Actividades:</Typography>
         {activities.map((activity, index) => (
           <div key={index}>
             <hr />
             <Grid container spacing={3} justifyContent="left" alignItems="center">
               <Grid item xs={5}>
-                <Button 
-                  component={Link}
-                  to={"/actividades/" + activity.id}
-                  key={"Actividad"}
-                  variant="outlined"
-                  color="secondary"
-                  sx={{ my: 2, color: '#372249', display: 'block' }}
-                >
-                <Typography variant="h2" sx={{ color: '#372249' }}>{activity.title}</Typography>
-                <Typography variant="p" sx={{ color: '#372249' }}>Creado por: {activity.author}</Typography>
-                </Button>
+              <Button 
+                component={Link}
+                to={"/actividades/" + activity.id}
+                key={"Actividad"}
+                variant="outlined"
+                color="secondary"
+                sx={{ my: 2, color: '#372249', display: 'block' }}
+              >
+                <Box flexDirection="row" display="flex" justifyContent="space-between" alignItems="center" width="100%">
+                  <Typography component={'span'} variant={'h4'} sx={{ color: '#372249', mr: 2 }}>{activity.title}</Typography>
+                  <Typography variant="p" sx={{ color: '#372249', mr: 2 }}>
+                    Fecha de la actividad: {
+                      format(new Date(activity.dateActivity), 'd/M/yyyy')
+                    }
+                  </Typography>
+                  <Typography variant="p" sx={{ color: '#372249' }}>Participantes: {activity.members}</Typography>
+                </Box>
+              </Button>
               </Grid>
               <Grid item xs={2}>
                 <Button
