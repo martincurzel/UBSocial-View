e9501ea7825afffc03daceb6d0b92897fb933d7e
mcurzel <martin.curzel@contractors.lesschwab.com>
2023-07-03 05:12:26 -0300

    Update ActLista.js

--- src/components/ActLista.js
+++ src/components/ActLista.js
@@ -56,7 +56,7 @@ const ActLista = (props) => {
                 {act.isJoined === null ? (
                 <>
                 </>
-                ) : act.isJoined === false ? (
+                ) : act.isJoined == false ? (
                 <>
                   <Button onClick={() => handleJoin(act)} size="small" color="primary">
                     unirme
