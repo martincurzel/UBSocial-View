2c78a28bb9b91c5cee02520bb836bd900dfc7c76
Nicolas Margossian <nmargo@pm.me>
2023-07-03 01:10:44 -0300

    diseño

--- src/components/ActLista.js
+++ src/components/ActLista.js
@@ -1,31 +1,53 @@
 import { Link } from "react-router-dom";
-import { Button } from "@mui/material";
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import { Button, CardActionArea, CardActions } from '@mui/material';
 
 
 const ActLista = (props) => {
-    
-    return (
-        <div className="act-lista">
-            <h2>{props.title}</h2>
-            {props.acts.map((act)=> (
-          <div className="act-preview" key={act.id}>
 
-            <Button 
-              component={Link}
-                to={"/actividades/" + act.id}
-                key={"Crear Actividad"}
-                sx={{ my: 2, color: 'black', display: 'block' }}
-              >
-               <h2>{act.title}</h2>
-               <p>Fecha de finalizacion: {act.ActivityDateFinished}</p>
+  const BASE_PATH = process.env.REACT_APP_CONTROLLER_API_BASE_PATH;
 
-              
-            </Button>
-            
+
+  return (
+    <section className="row">
+      {
+        props.acts.map((act) => (
+
+          <div className="col-md-3 mt-5">
+            <Card className="" key={act.id} sx={{ maxWidth: 345 }}>
+              <CardActionArea component={Link} to={"/actividades/" + act.id}>
+                <CardMedia
+                  component="img"
+                  height="140"
+                  image={BASE_PATH + act.URLPhotos}
+                  alt="Foto"
+                />
+                <CardContent>
+                  <Typography gutterBottom variant="h5" component="div">
+                    {act.title}
+                  </Typography>
+                  <Typography variant="body2" color="text.secondary">
+                    Fecha de finalizacion: {act.dateFinishActivity}
+                  </Typography>
+                </CardContent>
+              </CardActionArea>
+              <CardActions>
+                <Button component={Link} to={"/actividades/" + act.id} size="small" color="primary">
+                  ver
+                </Button>
+              </CardActions>
+            </Card>
           </div>
-            ))}
-        </div>
-    );
+
+        ))
+      }
+
+    </section>
+
+  )
 }
- 
+
 export default ActLista;
